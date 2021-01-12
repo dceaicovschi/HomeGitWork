e38aa122a7d1a65fd6fed84b1beb3f4f1f4f8ef5
dceaicovschi <dumitru.ceaicovschi@endava.com>
2021-01-12 17:53:52 +0200

    HomeWorkTask2

---
+++ src/main/java/GitTestClass.java
@@ -0,0 +1,10 @@
+public class GitTestClass {
+    public static void main(String[] args) {
+        int a = 2;
+        String git = "Git Home Works";
+        System.out.println(returnText(git));
+    }
+    public static String returnText(String str){
+        return str;
+    }
+}
---
+++ src/test/java/TestClassForGit.java
@@ -0,0 +1,8 @@
+public class TestClassForGit {
+
+    boolean b = true;
+
+    boolean testo(){
+        return b;
+    }
+}
